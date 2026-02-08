c7df6ea904895f65c7b116b278e6e0c4b4a41db0
dileep <yarramanenidileep@gmail.com>
2026-02-08 20:08:29 +0530

    BASIC SETUP COMPLETED

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import express from "express"
+const app=express()
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
